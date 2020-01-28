b44ee6755c9d670e180d80899d439df956ccbf27
Elom Amouzou <elom1990@yahoo.com>
2020-01-28 16:31:35 -0700

    create route for shelters/new

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
 
   get '/shelters', to: 'shelters#index'
+  get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
 end
