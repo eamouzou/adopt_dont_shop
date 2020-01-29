556cdddece284673e9f5e4bdfa193121055fc6f8
Elom Amouzou <elom1990@yahoo.com>
2020-01-28 18:58:35 -0700

    add two tests to see new shelter on index page, and click it to see info

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -32,6 +32,40 @@ RSpec.describe "create shelter", type:feature do
     expect(page).to have_field('shelter_state')
     expect(page).to have_field('shelter_zip')
     expect(page).to have_button('Submit')
-    save_and_open_page
+    # save_and_open_page
+  end
+
+  it "can fill out form, submit it, see its name on Shelter index page" do
+    visit "/shelters/new"
+
+    fill_in "shelter_name", with: "Spot"
+    fill_in "shelter_address", with: "5378 W. Curry St."
+    fill_in "shelter_city", with: "Westminster"
+    fill_in "shelter_state", with: "CO"
+    fill_in "shelter_zip", with: "80019"
+    click_button 'Submit'
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_content("Spot")
+  end
+
+  it "can fill out form, submit, click on the name link, and see all info" do
+    visit "/shelters/new"
+
+    fill_in "shelter_name", with: "Spot"
+    fill_in "shelter_address", with: "5378 W. Curry St."
+    fill_in "shelter_city", with: "Westminster"
+    fill_in "shelter_state", with: "CO"
+    fill_in "shelter_zip", with: "80019"
+    click_button 'Submit'
+
+    expect(current_path).to eq('/shelters')
+    expect(page).to have_link("Spot")
+    click_link 'Spot'
+    expect(page).to have_content("Spot")
+    expect(page).to have_content("5378 W. Curry St.")
+    expect(page).to have_content("Westminster")
+    expect(page).to have_content("CO")
+    expect(page).to have_content("80019")
   end
 end
