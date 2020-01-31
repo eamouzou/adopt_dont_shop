de5bd1a7d15d3dbc32fb14715d06bd556aa42bed
Elom Amouzou <elom1990@yahoo.com>
2020-01-31 16:05:50 -0700

    add new and create actions to pets controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -7,7 +7,16 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
   end
 
-  def create
+  def new
     @shelter = Shelter.find(params[:id])
+  end
+
+  def create
+    pet = Pet.create(name: params[:pet_name],
+      description: params[:pet_description],
+      approximate_age: params[:pet_approximate_age],
+      sex: params[:pet_sex], adoption_status: "adoptable",
+      shelter_id: params[:id], shelter_name: Shelter.find(params[:id]).name)
+    redirect_to "/shelters/#{params[:id].to_i}/pets"
   end
 end
