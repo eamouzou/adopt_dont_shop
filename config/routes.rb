bb17c405f83fcb461a6a5fdca0811397c32e5440
Elom Amouzou <elom1990@yahoo.com>
2020-01-31 16:04:34 -0700

    add routes for filling out form, and submitting it

--- config/routes.rb
+++ config/routes.rb
@@ -11,9 +11,10 @@ Rails.application.routes.draw do
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
   get '/shelters/:id/pets', to: 'shelters#display'
-  get '/shelters/:id/pets/new', to: 'pets#create'
 
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
+  get '/shelters/:id/pets/new', to: 'pets#new'
+  post '/shelters/:id/pets', to: 'pets#create'
 end
