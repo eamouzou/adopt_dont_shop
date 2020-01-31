38b96488a65aaeb425933e0928a57791ab49969d
Elom Amouzou <elom1990@yahoo.com>
2020-01-31 08:49:43 -0700

    commented out unneeded catgeories from seed objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,18 +13,20 @@ city: "Hershey", state: "CO", zip: "17033")
 
 athena = Pet.create(image: "https://images-ra.adoptapet.com/images/Homepage-DogV2.png",
 name: "Athena",
-description: "butthead",
+# description: "butthead",
 approximate_age: 1,
 sex: "female",
-status: "adoptable",
-shelter_name: "Mike's Shelter")
+# status: "adoptable",
+shelter_name: "Mike's Shelter",
+shelter_id: mikes_shelter.id)
 
 odell = Pet.create(image: "https://imgix.bustle.com/uploads/getty/2019/11/18/6296727a-d38c-40b4-8ffe-dbec5cd1b289-getty-954967324.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format&q=70",
 name: "Odell",
-description: "good dog",
+# description: "good dog",
 approximate_age: 4,
 sex: "male",
-status: "adoptable",
-shelter_name: "Meg's Shelter")
+# status: "adoptable",
+shelter_name: "Meg's Shelter",
+shelter_id: megs_shelter.id)
 
 #run this with 'rails db:seed' after pet show
