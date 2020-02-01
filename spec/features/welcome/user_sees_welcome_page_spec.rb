8849df57ef26812879fcb341cff8d0d8f98da11d
Elom Amouzou <elom1990@yahoo.com>
2020-01-31 19:40:35 -0700

    add test for welcome page links

--- spec/features/welcome/user_sees_welcome_page_spec.rb
+++ spec/features/welcome/user_sees_welcome_page_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe "shelters welcome", type: feature do
 
     expect(page).to have_content("Welcome to Adopt, Don't Shop!")
   end
+
+  it "sees links to pets and shelters" do
+    visit "/"
+
+    expect(page).to have_link("All Shelters")
+    expect(page).to have_link("All Pets")
+  end
+
+  it "clicks link and goes to shelters" do
+    visit "/"
+    click_link "All Shelters"
+
+    expect(current_path).to eq('/shelters')
+  end
+
+  it "clicks link and goes to pets" do
+    visit "/"
+    click_link "All Pets"
+
+    expect(current_path).to eq('/pets')
+  end
 end
